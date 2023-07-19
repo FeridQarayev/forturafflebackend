a85188407b67935acdf020ff0712d17078feb4b6
FeridQarayev <feridqarayev111@gmail.com>
2023-07-19 10:20:18 +0400

    test git credential

--- server/validators/user.validator.js
+++ server/validators/user.validator.js
@@ -4,7 +4,7 @@ exports.registerValSchema = Joi.object({
   firstName: Joi.string().required(),
   lastName: Joi.string().required(),
   email: Joi.string().email().required(),
-  password: Joi.string().required(),
+  password: Joi.string().required() ,
 });
 
 exports.loginValSchema = Joi.object({
